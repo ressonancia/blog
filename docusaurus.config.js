97db4d4de35928c00b9fb719770a5a6211be0a42
Victor Gazotti <vtr.gomes@hotmail.com>
2025-12-03 22:24:20 -0300

    starting blog

---
+++ docusaurus.config.js
@@ -0,0 +1,116 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Ressonance Blog',
+  tagline: 'Websockets made cheap, easy and fast',
+  favicon: 'img/favicon.ico',
+
+  future: {
+    v4: true,
+  },
+
+  // url: 'https://blog.ressonance.com',
+  url: 'https://ressonancia.github.io',
+  baseUrl: '/',
+  organizationName: 'ressonancia',
+  projectName: 'ressonance-blog',
+  trailingSlash: false,
+  deploymentBranch: 'master',
+  onBrokenLinks: 'throw',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        blog: {
+          routeBasePath: '/',
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: 'img/docusaurus-social-card.jpg',
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
+      navbar: {
+        title: 'Ressonance Blog',
+        logo: {
+          alt: 'Ressonance Blog Logo',
+          src: 'img/logo.png',
+          href: '/',
+        },
+        items: [
+          {
+            href: 'https://github.com/ressonancia',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Visit ressonance docs',
+                to: 'https://docs.ressonance.com',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'X',
+                href: 'https://x.com/@RessonanceCom',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/ressonancia',
+              },
+              {
+                label: 'Blog',
+                href: '/',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Ressonance.com`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
